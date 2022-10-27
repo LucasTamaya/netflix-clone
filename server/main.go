e3be0a00ff262f5f75fba7016b75f5da610b14ca
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-27 08:37:47 +0400

    init golang server
    - install fiber package
    - app running on port 8080

--- server/main.go
+++ server/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
+	app := fiber.New()
 
-	fmt.Println("Hello world")
+	// handle CORS error
+	app.Use(cors.New())
 
+	log.Fatal(app.Listen(":8080"))
 }
