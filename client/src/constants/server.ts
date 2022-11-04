967d046955aeb914d7206779e377697f5c42e24f
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-04 15:11:08 +0400

    updates PROD_URL

--- client/src/constants/server.ts
+++ client/src/constants/server.ts
@@ -1,5 +1,5 @@
 const DEV_URL = "http://localhost:8080";
-const PROD_URL = "https://netflix-clone-api-production.up.railway.app/";
+const PROD_URL = "https://netflix-clone-api-production.up.railway.app";
 
 export const SERVER_BASE_URL =
   process.env.NODE_ENV === "development" ? DEV_URL : PROD_URL;
