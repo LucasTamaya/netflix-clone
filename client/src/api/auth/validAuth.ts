c1c80e1ba7d0a3aaedc8dd7cb5970c7926bc487f
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-06 18:21:54 +0400

    deleted jwtConfig
    - because when a user logout, the tokenvalue in localStorage = null, but
      it uses it for the config, so the next request fail

--- client/src/api/auth/validAuth.ts
+++ client/src/api/auth/validAuth.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 import { SERVER_BASE_URL } from "../../constants/server";
-import { config } from "../jwtConfig";
 
 export const handleValidAuth = async () => {
-  const { data } = await axios.get(`${SERVER_BASE_URL}/auth/valid`, config);
+  const { data } = await axios.get(`${SERVER_BASE_URL}/auth/valid`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 
   return data;
 };
--- client/src/api/user/getUserProfileData.ts
+++ client/src/api/user/getUserProfileData.ts
@@ -2,12 +2,15 @@ import axios from "axios";
 
 import { SERVER_BASE_URL } from "../../constants/server";
 import { ApiUserProfileDataResponse } from "../../types";
-import { config } from "../jwtConfig";
 
 export const getUserProfileData = async () => {
   const { data } = await axios.get<ApiUserProfileDataResponse>(
     `${SERVER_BASE_URL}/user/profile`,
-    config
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
   );
 
   return data;
