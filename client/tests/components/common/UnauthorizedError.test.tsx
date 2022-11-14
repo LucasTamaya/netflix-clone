fbbab43fe02e4efdc68e64918194092b497566ac
LucasTamaya <lucastamaya.974@gmail.com>
2022-11-14 09:21:10 +0400

    added unit tests for Error component

---
+++ client/tests/components/common/UnauthorizedError.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+
+import { UnauthorizedError } from "~src/components/common/UnauthorizedError";
+
+describe("UnauthorizedError Component", () => {
+  it("should renders the component correctly", () => {
+    render(<UnauthorizedError />);
+
+    const title = screen.getByRole("heading", { name: /Unauthorized access/i });
+    const errMsg = screen.getByRole("heading", {
+      name: /you are going to be redirected/i,
+    });
+
+    expect(title).toBeInTheDocument();
+    expect(errMsg).toBeInTheDocument();
+  });
+});
---
+++ client/tests/components/common/UnknownError.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import { UnknownError } from "~src/components/common/UnknownError";
+
+describe("UnauthorizedError Component", () => {
+  it("should renders the component correctly", () => {
+    render(<UnknownError />);
+
+    const title = screen.getByRole("heading", {
+      name: /oops, something went wrong.../i,
+    });
+    const errMsg = screen.getByRole("heading", {
+      name: /try to reload the page/i,
+    });
+
+    expect(title).toBeInTheDocument();
+    expect(errMsg).toBeInTheDocument();
+  });
+});
